6848bd87e6ab61d3ace606e1e7595efcd85d4e75
Shinichi Okada <okada.shin@gmail.com>
2023-02-08 11:28:06 +0900

    fix: add / to links

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  root: '.',
-  build: {
-    outDir: './docs'
-  }
+  plugins: [svelte()]
 })
