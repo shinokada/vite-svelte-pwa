44e1c69cd7ad361250cac0f2ec719af594b56a13
Shinichi Okada <okada.shin@gmail.com>
2023-02-08 12:22:20 +0900

    docs: update README

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    assetsInlineLimit: 0, // 0 means all files will be inlined
+  },
 })
